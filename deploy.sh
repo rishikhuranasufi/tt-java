017f3c4f41b2c19ab2a8702239b9f9607f30e54d
ajithreddy444 <32986843+ajithreddy444@users.noreply.github.com>
2021-03-10 16:03:39 +0530

    Adding deploy commands

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 echo " killing Process"
 ps -ef| grep app1.jar| grep -v grep| awk '{print $2}'| xargs kill -9
 export BUILD_ID=dontKillMe
-echo " running jar"
+echo " running app1.jar"
 nohup java -jar ~/app1.jar > ~/applogs.log 2>&1 &
 echo " Deployment Finished"
