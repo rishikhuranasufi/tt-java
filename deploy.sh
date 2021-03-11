524ef1c338bda2d8e9489915226c733aa0ac4304
rishikhuranasufi <rishikhurana.sufi@gmail.com>
2021-03-11 09:11:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo " killing Process"
-ps -ef| grep app1.jar| grep -v grep| awk '{print $2}'| xargs kill -9
+ps -ef| grep app.jar| grep -v grep| awk '{print $2}'| xargs kill -9
 export BUILD_ID=dontKillMe
-echo " running app1.jar"
-nohup java -jar ~/app1.jar > ~/applogs.log 2>&1 &
+echo " running app.jar"
+nohup java -jar ~/app.jar > ~/applogs.log 2>&1 &
 echo " Deployment Finished"
